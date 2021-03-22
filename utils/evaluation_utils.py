c39b65f21afbf3b8ef88c0605d30e367688be692
swel4ik <zadorojniy.sanya@yandex.ru>
2021-03-22 17:33:01 +0300

    add comments

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -7,12 +7,18 @@ def check_arr(a: list, value: np.ndarray) -> bool:
     for k in range(len(a)):
         if a[k][0] == value[0] and a[k][1] == value[1] and a[k][2] == value[2]:
             return True
-
     return False
 
 
 @numba.njit()
 def get_colors_pack(img: np.ndarray) -> list:
+    """
+    Get unique colors from image
+    Args:
+        img: RGB image
+    Returns:
+        List of unique colors
+    """
     colors_pack = []
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
@@ -24,17 +30,32 @@ def get_colors_pack(img: np.ndarray) -> list:
 
 @numba.njit()
 def get_equal_indexes(img: np.ndarray, value: np.ndarray) -> list:
+    """
+    Get color's class coordinates
+    Args:
+        img: RGB image
+        value: RGB color value
+    Returns:
+        List of color class coordinates
+    """
     eq_indexes = []
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             cl = img[i, j]
             if cl[0] == value[0] and cl[1] == value[1] and cl[2] == value[2]:
                 eq_indexes.append([i, j])
-
     return eq_indexes
 
 
 def multidim_intersect(arr1: np.ndarray, arr2: np.ndarray) -> np.ndarray:
+    """
+    Get intersection value, multi-dim
+    Args:
+        arr1: the first array (dim > 1)
+        arr2: the second array (dim > 1)
+    Returns:
+        Intersection value of 2 arrays
+    """
     arr1_view = arr1.view([('', arr1.dtype)] * arr1.shape[1])
     arr2_view = arr2.view([('', arr2.dtype)] * arr2.shape[1])
     intersected = np.intersect1d(arr1_view, arr2_view)
@@ -46,6 +67,14 @@ def get_unique_colors(img: np.ndarray, counts: bool = False):
 
 
 def sort_classes(colors: np.ndarray, rates: np.ndarray) -> list:
+    """
+    Sort cover rates of each color for estimation
+    Args:
+        colors: array with all colors in image
+        rates: array with cover rates of each color
+    Returns:
+        Sorted list of colors cover rates
+    """
     all_classes = []
     for i in range(len(rates)):
         tup = (colors[i], rates[i])
